8c2e8b23edfe8735d5ca25a29cd9cb58f963ea03
niki5M <meshkova.n.i.22@gmail.com>
2023-11-27 16:24:00 +0300

    8.2

--- donut/app.js
+++ donut/app.js
@@ -8,6 +8,8 @@ mongoose.connect('mongodb://localhost/donutBD')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var donuts = require('./routes/donuts');
+
 
 var app = express();
 
@@ -24,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/donuts', donuts);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
