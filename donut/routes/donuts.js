f0e3fc122ee8700447a681dafa64ecb7d1df99c3
niki5M <meshkova.n.i.22@gmail.com>
2023-11-27 18:17:42 +0300

    8.3

--- donut/routes/donuts.js
+++ donut/routes/donuts.js
@@ -1,13 +1,27 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const Donut = require("../models/donut").Donut;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.send('Новый маршрутизатор, для маршрутов, начинающихся с donuts');
 });
 
-router.get("/:nick", function(req, res, next) {
-  res.send(req.params.nick);
-  });
+router.get("/:nick", async (req, res, next) => {
+  try {
+    const donut = await Donut.findOne({ nick: req.params.nick });
+    console.log(donut);
+    if (!donut) {
+      throw new Error("Нет такого пончика!!");
+    }
+    res.render('donut', {
+      title: donut.title,
+      picture: donut.avatar,
+      desc: donut.desc
+    });
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
