0dc5145bd3649a43ea26bbadc5894164783fa283
niki5M <meshkova.n.i.22@gmail.com>
2023-11-27 15:55:27 +0300

    7.2

--- donut/mongooseTasks.js
+++ donut/mongooseTasks.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/test');
 
-const Donut = mongoose.model('Donut', { name: String });
+var schema = mongoose.Schema({ name: String })
+schema.methods.yummy = function(){
+  console.log(this.get("name") + " сказал вкусно")
+  }
+  
+const Donut = mongoose.model('Donut', schema);
 
 const doughnut = new Donut({ name: 'СуперПончик' });
-doughnut.save().then(() => console.log('вкусно'));
+doughnut.save().then(() => doughnut.yummy());
