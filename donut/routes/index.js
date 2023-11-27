c17ab7bc8aff1e21c589423f000580bee60ef6b2
niki5M <meshkova.n.i.22@gmail.com>
2023-11-27 18:51:43 +0300

    8.4

--- donut/routes/index.js
+++ donut/routes/index.js
@@ -5,29 +5,5 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
-router.get('/pink', function(req, res, next) {
-  res.render('donut', {
-  title: "Розовый",
-  picture: "images/donut1.png",
-  desc: "Пончик с клубничной начинкой"
-  });
-});
-
-router.get('/white', function(req, res, next) {
-    res.render('donut', {
-    title: "Белый",
-    picture: "images/donut2.png",
-    desc: "Пончик с молочной начинкой"
-  });
-});
-
-router.get('/brown', function(req, res, next) {
-      res.render('donut', {
-      title: "Коричневый",
-      picture: "images/donut3.png",
-      desc: "Пончик с шоколадной начинкой"
-  });
-});
       
 module.exports = router;
