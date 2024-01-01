fdd316dca55e17fb282c3d476a7606b5170cd123
Niki <meshkova.n.i.22@gmail.com>
2024-01-01 23:25:23 +0300

    10.5

--- donut/app.js
+++ donut/app.js
@@ -43,6 +43,7 @@ app.use(function(req,res,next){
   })
   
 app.use(require("./middleware/createMenu.js"));
+app.use(require("./middleware/createUser.js"));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/donuts', donuts);
