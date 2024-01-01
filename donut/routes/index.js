c69a9ae4ef01ea99e2df229a648caec070d16a86
Niki <meshkova.n.i.22@gmail.com>
2024-01-01 21:03:29 +0300

    10.1

--- donut/routes/index.js
+++ donut/routes/index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 const Donut = require("../models/donut").Donut;
 
+/* GET login/registration page. */
+router.get('/logreg', function(req, res, next) {
+  res.render('logreg',{title: 'Вход'});
+  });
+
 /* GET home page. */
 router.get('/', async(req, res, next) => {
   try{
