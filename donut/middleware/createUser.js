c58c33e24a6013f6f4add9ace5d2ad93e59346fc
Niki <meshkova.n.i.22@gmail.com>
2024-01-04 22:49:44 +0300

    11.7

--- donut/middleware/createUser.js
+++ donut/middleware/createUser.js
@@ -1,11 +1,23 @@
-const User = require("../models/user").User;
+var db = require('../mySQLConnect.js');
+
 
 module.exports = function(req,res,next) {
 res.locals.user = null
+console.log('Проверяем сессию')
+console.log(req.session.user)
+db.query(`SELECT * FROM user WHERE user.id = '${req.session.user}'`, (err, users) => {
+if(err)
+return next(err)
+if(users.length > 0) {
+res.locals.user = users[0];
+}
+next()
+})
+
 // User.findById(req.session.user, function (err, user) { // if (err)
 // return next(err)
 // res.locals.user = user;
 // next();
 // })
-next();
+// next();
 }
